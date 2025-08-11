8014afc4d26b9311d66172687dfa3ad339ab49cb
Student <grant.james.edwards@gmail.com>
2025-08-11 16:41:13 -0700

    ahhhh

--- iDRaylibTemplate (4)/iDRaylibTemplate/classes.h
+++ iDRaylibTemplate (4)/iDRaylibTemplate/classes.h
@@ -4,28 +4,13 @@
 class Item
 {
     public:
-    Texture2D image = LoadTexture("images/png/ammo.png");
+    Texture2D image = LoadTexture("images/coin.png");
     Vector2 position;
     Rectangle collider;
     bool collect = false;
     float size = 54;
 
     Item(float x, float y)
-    {
-        position = {x,y};
-        collider = {x, y, size, size};
-    }
-};
-
-class Wall
-{
-    public:
-    Texture2D image = LoadTexture("images/png/workbenchSmall (1) (7).png");
-    Vector2 position;
-    Rectangle collider;
-    float size = 64;
-
-    Wall(float x, float y)
     {
         position = {x,y};
         collider = {x, y, size, size};
---
+++ iDRaylibTemplate (4)/iDRaylibTemplate/game.cpp
@@ -0,0 +1,173 @@
+
+// DONUT LAND INVASION
+// A Top-Down Action-Adventure Game
+// Created by Eevee
+
+#include "raylib.h"
+#include "classes.h"
+
+// ===================================================================================================================
+int main()
+{
+    // Initialize Window
+    int screenWidth = 800;
+    int screenHeight = 450;
+    InitWindow(screenWidth, screenHeight, "Top-Down Game");
+
+    // Variables
+    int score = 0;
+
+    // Initialize Background
+    Texture2D bgImage = LoadTexture("images/level1.png");
+
+    // Initialize Player
+    //Texture2D playerImage = LoadTexture("images/player.png");
+    float playerX = 500;
+    float playerY = 100;
+    Vector2 player = {playerX, playerY};
+    float speed = 4.0f;
+    Rectangle playerCollider = {player.x, player.y, 40, 40};
+
+    // Player Direction
+    Texture2D playerRight = LoadTexture("images/player_right.png");
+    Texture2D playerLeft = LoadTexture("images/player_left.png");
+    Texture2D playerUp = LoadTexture("images/player_up.png");
+    Texture2D playerDown = LoadTexture("images/player_down.png");
+    typedef enum
+    {
+        up,
+        down,
+        left,
+        right
+    } Direction;
+    Direction playerDir = down;
+
+    // Item Initialization
+    /*Texture2D coinImage = LoadTexture("images/coin.png");
+    Vector2 coin = {100, 200};
+    Rectangle coinCollider = {coin.x, coin.y, 50, 50};
+    bool collect = false;*/
+    bool coinCollision = false;
+    Item items[3] = {Item(100, 100), Item(100, 200), Item(100, 300)};
+
+    SetTargetFPS(60);
+    // ================================================================================================================
+    // Game Loop
+    while (!WindowShouldClose())
+    {
+        //-------------------------------------------------
+        // Player Controls
+        if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D))
+        {
+            player.x += speed;
+            playerDir = right;
+        }
+        if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A))
+        {
+            player.x -= speed;
+            playerDir = left;
+        }
+        if (IsKeyDown(KEY_UP) || IsKeyDown(KEY_W))
+        {
+            player.y -= speed;
+            playerDir = up;
+        }
+        if (IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S))
+        {
+            player.y += speed;
+            playerDir = down;
+        }
+        // Set the Collider's Position
+        playerCollider.x = player.x;
+        playerCollider.y = player.y;
+
+        //----------------------
+        // Boundaries for the Player Movement
+        if ((player.x + 64) >= screenWidth) // Right
+        {
+            player.x = (screenWidth - 64);
+        }
+        if (player.x <= 0) // Left
+        {
+            player.x = 0;
+        }
+        if (player.y <= 50) // Up
+        {
+            player.y = 50;
+        }
+        if ((player.y + 64) >= screenHeight) // Down
+        {
+            player.y = (screenHeight - 64);
+        }
+
+        //----------------------
+        // Coin Collision (Old)
+        /*if (collect == false)
+        {
+            coinCollision = CheckCollisionRecs(playerCollider, coinCollider);
+            if (coinCollision == true)
+            {
+                collect = true;
+            }
+        }*/
+
+        //-------------------------------------------------
+        BeginDrawing();
+
+        // Draw Background
+        DrawTexture(bgImage, 0, 0, WHITE);
+
+        //----------------------
+        // Coin Collision
+        for (int i = 0; i < 3; ++i)
+        {
+            if (items[i].collect == false)
+            {
+                coinCollision = CheckCollisionRecs(playerCollider, items[i].collider);
+                if (coinCollision == true)
+                {
+                    score += 10;
+                    items[i].collect = true;
+                }
+                else
+                {
+                    DrawTexture(items[i].image, items[i].position.x, items[i].position.y, WHITE);
+                }
+            }
+        }
+
+        // Draw the Coin (Old)
+        /*if (collect == false)
+        {
+            DrawTexture(coinImage, coin.x, coin.y, WHITE);
+        }*/
+
+        //----------------------
+        // Draw Score
+        DrawRectangle(0, 0, screenWidth, 50, BLACK);
+        DrawText(TextFormat("Score: %i", score), 10, 10, 30, WHITE);
+
+        // Draw the Player
+        switch (playerDir)
+        {
+            case right:
+                DrawTexture(playerRight, player.x, player.y, WHITE);
+            break;
+            case left:
+                DrawTexture(playerLeft, player.x, player.y, WHITE);
+            break;
+            case up:
+                DrawTexture(playerUp, player.x, player.y, WHITE);
+            break;
+            case down:
+                DrawTexture(playerDown, player.x, player.y, WHITE);
+            break;
+            default:
+            break;
+        }
+
+        EndDrawing();
+        //-------------------------------------------------
+    }
+    CloseWindow();
+}
